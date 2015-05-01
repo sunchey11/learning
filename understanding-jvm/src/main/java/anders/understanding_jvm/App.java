7a8b463223d2d7b25eabde38c8b4f34cc84c16df
anders sun <23386983@qq.com>
2015-05-01 21:59:24 +0800

    add jam

---
+++ understanding-jvm/src/main/java/anders/understanding_jvm/App.java
@@ -0,0 +1,13 @@
+package anders.understanding_jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
