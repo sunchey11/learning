9eea9ad3e7d66382edaeb334e6f34cdb8888dbea
sunchey11 <23386983@qq.com>
2015-07-23 18:05:13 +0800

    add dyna array

--- CardGame/CardGame/CardGame.cpp
+++ CardGame/CardGame/CardGame.cpp
@@ -5,20 +5,63 @@
 #include <iostream>
 #include <string>
 #include "Card.h"
+#include "DynaArray.h"
 using std::cout;
 using std::cin;
 using std::endl;
 
-void gameStart(){
-	cout<<"gameStart"<<endl;
-}
+
 void testCard_1(){
 	cout<<"testCard_1"<<endl;
 	Card c(70);
 	cout<<"type:"<<c.getType()<<",num:"<<(int)c.getNumber()<<endl;
 
 }
+void testDynaArray_2(){
+	cout<<"testDynaArray_2"<<endl;
 
+	//测试普通构造函数
+	DynaArray <int> arr1;
+	cout<<arr1.max<<endl;
+	arr1.add(5);
+
+	//测试普通构造函数
+	DynaArray <int> arr2(10);
+	cout<<arr2.max<<endl;
+	arr2.add(15);
+
+	//测试复制构造函数
+	DynaArray <int> arr3(arr1);
+	cout<<arr3.max<<endl;
+
+	//测试赋值运算符
+	arr3 = arr2;
+	cout<<arr3.max<<endl;
+
+	//测试添加
+	try
+	{
+		for(int i=0;i<10;i++){
+
+			arr3.add(100+i);
+
+		}
+	}
+	catch(IndexOutOfBoundsException e)
+	{
+		cout<<"exxxxxxxxxxxxxxxxxxxxxxxx:"<<&e<<endl;
+	}
+
+	cout<<"okkk"<<endl;
+	arr3.debug();
+
+
+}
+void gameStart(){
+	cout<<"gameStart"<<endl;
+
+	testDynaArray_2();
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	const int max=10;
@@ -36,9 +79,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		}else if(intChoice==1)
 		{
 			testCard_1();
+		}else if(intChoice==2)
+		{
+			testDynaArray_2();
 		}
 
-		
+
 	}
 
 
--- CardGame/CardGame/DynaArray.h
+++ CardGame/CardGame/DynaArray.h
@@ -1,10 +1,129 @@
 #pragma once
+
+class IndexOutOfBoundsException
+{
+};
+
 template <class T>class DynaArray
 {
 public:
-	DynaArray(void);
+	//只有静态常量整型数据成员才可以在类中初始化
+	//const static int MAX=100; //这个可以
+	static int MAX;//静态成员必须在外面初始化
+	int max;
+	DynaArray(int max=MAX);
+	DynaArray<T>(const DynaArray<T>& aMess);
 	~DynaArray(void);
 
+	DynaArray<T> & operator =(const DynaArray<T> & arr);
 	int add(const T& ele);
+
+	//返回值声明为引用，此函数就可以作为lvalue了，不需要再写一个set方法了
+	//如果不希望作为lvalue使用，可以增加const关键字
+	T& get(int index);
+	void remove(int index);
+	void debug(){
+		for(int i=0;i<currentLength;i++){
+			cout<<i<<"          "<<(this->get(i))<<endl;
+		}
+	}
+private:
+
+	int currentLength;
+	T* datas;
+
+
+public:
+
+	int size(void)
+	{
+		return 0;
+	}
 };
 
+template <class T> int DynaArray<T>::MAX=100;
+
+template <class T>DynaArray<T>::DynaArray(int max)
+	: max(max),currentLength(0)
+{
+	datas = new T[max];
+}
+
+/**
+因为有动态内存分配，所以要实现复制构造函数
+*/
+template <class T> DynaArray<T>::DynaArray(const DynaArray<T>& arr):currentLength(0)
+{
+	cout<<"Copy onstructor called"<<endl;
+
+	max=arr.max;
+	datas = new T[arr.max];
+	for(int i=0;i<arr.currentLength;i++){
+		//T value = arr.get(i); //这行不能编译,因为arr为const,arr.get(i)为lvalue,编译器认为可以修改arr
+		T value = arr.datas[i];
+		this->add(value);
+	}
+}
+//为什么返回引用,有a=b=c这种情况和(a=b)=c这种情况
+//如果不返回引用,可以编译,但是不能正确的赋值
+template <class T> DynaArray<T> & DynaArray<T>::operator=(const DynaArray<T>& arr)
+{
+	cout<<"Assignment operator called"<<endl;
+	if(this == &arr)               // Check addresses, if equal
+		return *this;                  // return the 1st operand
+
+	// Release memory for 1st operand
+	delete[] datas;
+	max=arr.max;
+	currentLength=0;
+	datas = new T[arr.max];
+
+	for(int i=0;i<arr.currentLength;i++){
+
+		//T value = arr.get(i); //这行不能编译,因为arr为const,arr.get(i)为lvalue,编译器认为可以修改arr
+		T value = arr.datas[i];
+		add(value);
+	}
+	
+
+	// Return a reference to 1st operand
+	return *this;
+}
+
+template <class T>DynaArray<T>::~DynaArray(void)
+{
+	delete[] datas;
+}
+
+
+
+template <class T>	int DynaArray<T>::add(const T& ele)
+{
+	if(currentLength>=max){
+		throw IndexOutOfBoundsException();
+	}
+	datas[currentLength] = ele;
+	currentLength++;
+
+	return currentLength;
+}
+template <class T> T& DynaArray<T>:: get(int index)
+{
+	if(index>currentLength){
+		throw IndexOutOfBoundsException();
+	}
+	return datas[index];
+}
+
+
+template <class T>	void DynaArray<T>:: remove(int index)
+{
+	if(index>currentLength){
+		throw IndexOutOfBoundsException();
+	}
+	for(int i=index;i<currentLength;i++){
+		datas[i]=datas[i+1];
+	}
+	currentLength--;
+	return;
+}
