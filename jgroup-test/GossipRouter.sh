3d75f9137119f46e87409b6968e428a8dd0bceef
sunchey11 <23386983@qq.com>
2016-08-04 20:33:17 +0800

    port

--- jgroup-test/GossipRouter.sh
+++ jgroup-test/GossipRouter.sh
@@ -1,1 +1,2 @@
-java org.jgroups.stack.GossipRouter -port 12001
+#java org.jgroups.stack.GossipRouter -port 12001
+mvn exec:java -Dexec.mainClass="org.jgroups.stack.GossipRouter" -Dexec.args="-port 12001"
